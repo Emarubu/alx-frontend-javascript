ebdfc399c474dcd09ea60d83213722f9be6529b6
Emarubu <emarubu945@gmail.com>
2023-07-04 11:19:15 +0300

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
-function getListStudents(){
+export default function getListStudents(){
     return [
         {id: 1, firstname: 'Guillaume', location: 'San Francisco'},
         {id: 2, firstname: 'James', location: 'Columbia'},
-       {id: 5, firstname: 'Serena', location: 'San Francisco'}
-    
-    ]
+       {id: 5, firstname: 'Serena', location: 'San Francisco'},
+    ];
 
 }
